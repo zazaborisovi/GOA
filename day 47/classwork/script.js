8aaeca2f095394d0c72305f459624c8f620764b3
Zazaborisovi <geozedx123@mail.ru>
2024-03-17 15:58:20 +0400

    Commit

---
+++ day 47/classwork/script.js
@@ -0,0 +1,4 @@
+console.log(19 + 10)
+console.log(19 - 10)
+console.log(19 * 10)
+console.log(19 / 10)
---
+++ day 47/homework/script.js
@@ -0,0 +1,4 @@
+alert('Site Is Working Properly')
+let num1 = 100
+let num2 = 150
+console.log(num1 + num2)
