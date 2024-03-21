be4c5e7a4426906f973f3a4f0d961cc431c19650
Zazaborisovi <geozedx123@mail.ru>
2024-03-21 19:06:40 +0400

    Commit

---
+++ day 48/homework/script.js
@@ -0,0 +1,32 @@
+//js1
+const pi = 3.14
+const radius = 10
+const circle = pi * (radius * radius)
+console.log(circle)
+//js2
+let num1 = 5
+let num2 = 8
+console.log(num1 > num2)
+console.log(num1 >= num2)
+console.log(num1 < num2)
+console.log(num1 <= num2)
+console.log(num1 == num2)
+console.log(num1 != num2)
+//js3
+let bookname = "Harry Potter and the Sorcerers Stone"
+let pages = 223
+console.log(bookname + ' pages ' + String(pages))
+//js4
+let name = "zaza"
+let surname = "borisovi"
+let age = 16
+let city = "batumi"
+console.log('name: ' + name + ' surname: ' + surname + ' age: ' + String(age) + ' city: ' + city)
+//js5
+const num3 = 7
+const num4 = 12
+console.log(num3 + num4)
+console.log(num3 - num4)
+console.log(num3 * num4)
+console.log(num3 / num4)
+console.log(num3 % num4)
