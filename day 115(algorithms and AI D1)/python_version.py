c144a7e8bf38873ae02145210d729869ef5fe089
zazaborisovi <geozedx123@mail.ru>
2025-01-28 20:52:55 +0400

    Commit

--- day 115(algorithms and AI D1)/python_version.py
+++
@@ -1,5 +0,0 @@
-def problem1(a , b ,c):
-    if b > a and (b % c == 0 or a % c == 0):
-        return (b-a) // c + 1
-    else:
-        return (b-a) // c
--- projects/manage-landing-page-master/script.js
+++
@@ -1,8 +0,0 @@
-let x = document.getElementById("myLinks")
-
-if (x.style.display === "block") {
-  x.style.display = "none"
-} 
-else {
-  x.style.display = "block"
-}
