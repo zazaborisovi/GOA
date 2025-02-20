dc911403629b6ccf158370eec7d12cf840e862bd
zazaborisovi <geozedx123@mail.ru>
2025-02-20 21:19:27 +0400

    adding missing homeworks N2

--- day 107/homework/script.js
+++ day 107/homework/script.js
@@ -1,17 +1,24 @@
-const {mkdirSync , rmdirSync} = require("fs")
+const {mkdirSync , err , rmdirSync, writeFileSync , unlinkSync, readFileSync} = require("fs")
 
-const mkdirsFunc = () => {
-    for(let i = 0 ; i <= 10 ; i ++){
-        mkdirSync(`./directories/directory ${i}`)
+const path = "./dirs"
+
+function mkdirs() {
+    for (let i = 1 ; i <= 10 ; i ++ ){
+        mkdirSync(`${path}/directory ${i}`)
+        writeFileSync(`${path}/directory ${i}/idk.js` , "console.log('ixvi')")
         console.log(`directory ${i} created`)
     }
 }
-
-const rmdirsFunc = () => {
-    for(let i = 0 ; i <= 10 ; i ++){
-        rmdirSync(`./directories/directory ${i}`)
-        console.log(`directory ${i} removed`)
+function rmdirs(){
+    for(let i = 1 ; i <= 10 ; i ++){
+        unlinkSync(`${path}/directory ${i}/idk.js`);
+        rmdirSync(`${path}/directory ${i}`);
+        console.log(`directory ${i} deleted`)
     }
 }
-
-mkdirsFunc()
+function readfiles(){
+    for(let i = 1 ; i <= 10 ; i ++){
+        console.log(readFileSync(`${path}/directory ${i}/idk.js` , "utf-8" , (err) =>{if(err) throw err}))
+    }
+}
+readfiles()
